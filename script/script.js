6e468e5f071fae0e53e405ae5d29d8fe2d830122
chetan <=>
2018-11-10 11:04:54 +0530

    initial commit

---
+++ script/script.js
@@ -0,0 +1,225 @@
+let dataController = (function() {
+
+    let quotes = function(quote, author) {
+        this.quote = quote;
+        this.author = author
+    };
+
+    let quoteList = new Array();
+    
+    quoteList[0] = new quotes('It\'s not enough to be busy, so are the ants. The question is, what are we busy about?', '-- Henry David Thoreau');
+    quoteList[1] = new quotes('The key is in not spending time, but in investing it.', '-- Stephen R. Covey');
+    quoteList[2] = new quotes('Let our advance worrying become advance thinking and planning.', '-- Winston Churchill');
+    quoteList[3] = new quotes('Time is more valuable than money. You can get more money, but you cannot get more time.', '-- Jim Rohn');
+    quoteList[4] = new quotes('The shorter way to do many things is to only do one thing at a time.', '-- Mozart');
+    quoteList[5] = new quotes('Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin.', '-- Mother Teresa');
+    quoteList[6] = new quotes('Take care of the minutes and the hours will take care of themselves.', '-- Lord Chesterfield');
+
+    let taskList = new Array();
+    let data = function() {
+        this.desc = '';
+        this.id = -1;
+    };
+    let taskItem;
+    return {
+        storeItem(inputVal) {
+            taskItem = new data();
+
+            if(localStorage.getItem('taskList') !== null)
+            {
+                taskList = JSON.parse(localStorage.getItem('taskList'));
+            }
+
+            if(taskList.length !== 0)
+            {
+                taskItem.id = taskList[taskList.length - 1].id + 1;
+            }
+            else {
+                taskItem.id = 0;
+            }
+            taskItem.desc = inputVal;
+            taskList.push(taskItem);
+            localStorage.setItem('taskList', JSON.stringify(taskList));
+            return taskList[taskList.length - 1];
+        },
+
+        removeItem(ID) {
+            let itemIndex;
+            taskList = JSON.parse(localStorage.getItem('taskList'));
+            for(let i = 0; i < taskList.length; ++i)
+            {
+                if(taskList[i].id === Number(ID))
+                {
+                    itemIndex = i;
+                    break;
+                }
+            }
+            taskList.splice(itemIndex, 1);
+            if(taskList.length === 0) 
+            {
+                localStorage.removeItem('taskList');
+            }
+            else {
+                localStorage.setItem('taskList', JSON.stringify(taskList));
+            }
+        },
+
+        getQuote(quoteIndex) {
+            return quoteList[quoteIndex];
+        }
+    }
+})();
+
+let UIController = (function() {
+    let DOMEle = {
+        sectionTasks: '.section-tasks',
+        main: '.main',
+        resetBtn: '.btn--reset',
+        deleteBtn: '.task__delete-btn',
+        container: '.task',
+        quote: '.header__quote-quote',
+        author: '.header__quote-author'
+    }
+
+    return {
+
+        getDOMElements() {
+            return DOMEle;
+        },
+
+        noTaskScreen() {
+            let screen = '<div class="no-task-screen">Nothing to do..</div>';
+            let mainObj = document.querySelector(DOMEle.main);
+
+            // REMOVING 'section-tasks' FROM DOM.
+            document.querySelector(DOMEle.sectionTasks).style.display = 'none';
+            
+            // APPENDING 'no-task-screen' TO DOM.
+            mainObj.insertAdjacentHTML('beforeend', screen);
+        },
+
+        addTaskItem(data) {
+            document.querySelector(DOMEle.sectionTasks).style.display = 'block';
+            let taskItem = `<div class="task__item" id="task-${data.id}"><div class="task__item--left"><div class="task__delete" id="deleteBtn"><i class="icon-arrows-circle-remove"></i></div><div class="task__desc"><h3 class="heading-tertiary">${data.desc}</h3></div></div><div class="task__item--right"><div class="checkbox"><label class="checkbox__label" for="${data.id}"><input type="checkbox" class="checkbox__input" id="${data.id}"><span class="checkbox__custom"></span></label></div></div></div>`;
+
+            document.querySelector('.task').insertAdjacentHTML('beforeend', taskItem);
+            document.getElementById('eventInput').value = '';
+        },
+
+        deleteTaskItem(ItemID) {
+            document.querySelector(DOMEle.container).removeChild(document.getElementById('task-'+ItemID));
+        },
+
+        showQuote(quote)
+        {
+            document.querySelector(DOMEle.quote).textContent = quote.quote;
+            document.querySelector(DOMEle.author).textContent = quote.author;
+        }
+    }
+
+})();
+
+let controller = (function(dataCtrl, UIctrl) {
+
+    let DOMEle = UIctrl.getDOMElements();
+
+    let quotesHandler = function() {
+
+        let quoteIndex, quote;
+
+        // SELECT THE RANDOM QUOTE.
+        quoteIndex = Math.floor(Math.random()*7);
+        quote = dataCtrl.getQuote(quoteIndex);
+
+        // DISPLAY WITH TYPING EFFECT.
+        UIctrl.showQuote(quote);
+
+    }
+
+    let addItem = function() {
+        let inputVal, data;
+
+        // TAKE THE INPUT VALUE
+        inputVal = document.getElementById('eventInput').value;
+        if(inputVal !== '')
+        { 
+            // STORE THE VALUE IN AN OBJECT
+            data = dataCtrl.storeItem(inputVal);
+            
+            // UPDATE UI
+            if(JSON.parse(localStorage.getItem('taskList')).length === 1)
+            {
+                document.querySelector(DOMEle.main).removeChild(document.querySelector('.no-task-screen'));
+            }
+            UIctrl.addTaskItem(data);
+        }
+    }
+
+    let deleteItem = function(e) {
+        let taskID;
+
+        // SELECT ITEM FROM CONTAINER.
+        if(e.target.parentNode.id === 'deleteBtn')
+        {
+            taskID = e.target.parentNode.parentNode.parentNode.id;
+            
+            // DELETE DATA STRUCTURES.
+            taskID = taskID.split('-')[1];  
+            dataCtrl.removeItem(taskID);
+            
+            // DELETE FROM UI.
+            UIctrl.deleteTaskItem(taskID);
+            if(localStorage.getItem('taskList') === null)
+            {
+                UIctrl.noTaskScreen();
+            }
+        }
+    }
+
+    let setupEventListeners = function() {
+        let addbtn = document.getElementById('addBtn');
+        let inputField = document.getElementById('eventInput');
+        let reset = document.querySelector(DOMEle.resetBtn);
+
+        // ON BUTTON CLICK.
+        addbtn.addEventListener('click', addItem);
+
+        // ON PRESSING ENTER.
+        document.documentElement.addEventListener('keyup', function(e)
+        {
+            if(e.keyCode === 13 || e.which === 13)
+            {
+                addItem();
+            }
+        });
+
+        // ON CLICKING CLEAR ALL.
+        reset.addEventListener('click', function() {
+            localStorage.removeItem('taskList');
+            UIctrl.noTaskScreen();
+        });
+
+        // DELETING ONE ITEM FROM LIST.
+        document.querySelector(DOMEle.container).addEventListener('click', deleteItem);
+        
+
+    }
+    return {
+        init() {
+            if(localStorage.getItem('taskList') === null)
+            {
+                UIctrl.noTaskScreen();
+            }
+            else {
+                for(let i = 0; i < JSON.parse(localStorage.getItem('taskList')).length; ++i)
+                {
+                    UIctrl.addTaskItem(JSON.parse(localStorage.getItem('taskList'))[i]);
+                }
+            }
+            setupEventListeners();
+            quotesHandler();
+        }
+    }
+})(dataController, UIController);
+
+controller.init();
